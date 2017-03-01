1c7db11673234199e455c130955ab2dea59fe771
Steen F. Tøttrup <sft@creativeminds.dk>
2017-03-01 22:02:40 +0100

    Missing class for test assembly

---
+++ src/NForum.Tests.Core/TestUtils.cs
@@ -0,0 +1,19 @@
+﻿using NForum.CQS;
+using NSubstitute;
+using System;
+
+namespace NForum.Tests.Core {
+
+	public static class TestUtils {
+		public static IIdValidator GetInt32IdValidator() {
+			return new Int32IdValidator();
+		}
+
+		private class Int32IdValidator : IIdValidator {
+			public Boolean IsValid(String id) {
+				Int32 temp;
+				return Int32.TryParse(id, out temp);
+			}
+		}
+	}
+}
